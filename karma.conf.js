a73d43c7c2b2e81f4a36a80ac00fa8222d59c2a4
Richard Vivas Ordoñez <rvosistemas@github.com>
2024-08-23 16:45:21 -0500

    create config and test

---
+++ karma.conf.js
@@ -0,0 +1,39 @@
+module.exports = function (config) {
+  config.set({
+    frameworks: ['jasmine', '@angular-devkit/build-angular'],
+    plugins: [
+      require('karma-jasmine'),
+      require('karma-chrome-launcher'),
+      require('karma-jasmine-html-reporter'),
+      require('karma-coverage'),
+      require('@angular-devkit/build-angular/plugins/karma')
+    ],
+    client: {
+      clearContext: false
+    },
+    coverageReporter: {
+      dir: require('path').join(__dirname, './coverage/popayan-tourism-front'),
+      subdir: '.',
+      reporters: [
+        { type: 'html' },
+        { type: 'text-summary' }
+      ]
+    },
+    reporters: ['progress', 'kjhtml'],
+    port: 9876,
+    colors: true,
+    logLevel: config.LOG_INFO,
+    autoWatch: true,
+    browsers: ['ChromeHeadlessNoSandbox'],
+    customLaunchers: {
+      ChromeHeadlessNoSandbox: {
+        base: 'ChromeHeadless',
+        flags: ['--no-sandbox', '--disable-gpu', '--headless', '--remote-debugging-port=9222']
+      }
+    },
+    singleRun: false,
+    restartOnFileChange: true
+  });
+};
+
+console.log("Usando el archivo karma.conf.js");
--- src/app/domains/auth/components/login/login.component.spec.ts
+++ src/app/domains/auth/components/login/login.component.spec.ts
@@ -1,14 +1,29 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
 import { LoginComponent } from './login.component';
+import { AuthService } from '../../../../services/auth.service';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { FormBuilder } from '@angular/forms';
+import { Router } from '@angular/router';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations'; // Importar BrowserAnimationsModule
 
 describe('LoginComponent', () => {
   let component: LoginComponent;
   let fixture: ComponentFixture<LoginComponent>;
 
+  const routerSpy = jasmine.createSpyObj('Router', ['navigate']);
+
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [LoginComponent]
+      imports: [
+        LoginComponent,
+        HttpClientTestingModule,  // Provee HttpClient para las pruebas
+        BrowserAnimationsModule   // Provee soporte para animaciones
+      ],
+      providers: [
+        AuthService,   // Provee AuthService para el componente
+        FormBuilder,   // Provee FormBuilder para la creación de formularios
+        { provide: Router, useValue: routerSpy }  // Provee un mock de Router
+      ]
     })
       .compileComponents();
 
